3d695905cad6773c678f3fdf0908f35454866f07
Kilian Hayat <khayat@citycable.ch>
2021-03-22 17:18:32 +0100

    Configurate devise gen files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,6 +110,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  # defined default url options in your environments files
-  config.action_mailer.default_url_options = { host: 'https://waterbnb-580.herokuapp.com/', port: 443 }
+  config.action_mailer.default_url_options = { host: 'https://waterbnb-580.herokuapp.com/' }
+
 end
