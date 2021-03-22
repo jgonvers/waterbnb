ab2496756fc9cdea58e48a2174ca3ca1ce42716c
Kilian Hayat <khayat@citycable.ch>
2021-03-22 16:55:14 +0100

    defined default url options in your environments files for devises

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # defined default url options in your environments files
+  config.action_mailer.default_url_options = { host: 'https://waterbnb-580.herokuapp.com/', port: 443 }
 end
