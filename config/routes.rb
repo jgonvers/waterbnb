ddb510a1252539458ae25348b999fcf08193a56c
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-03-23 17:46:51 +0100

    added route for user homepage and homepage back/end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get "/user", to: "users#homepage"
   resources :pedalos, only: [:index, :show, :new, :create] do
     member do
       get 'reservations', to: 'reservations#index_res_pedalos'
