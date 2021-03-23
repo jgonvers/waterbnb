2ddbef41e15d9a773cffd124715cc86ecb182841
Kilian Hayat <khayat@citycable.ch>
2021-03-23 17:06:34 +0100

    Creaating the reservation/new backend

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -10,9 +10,29 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @date = Time.now.strftime("%B %d, %Y")
+    @pedalo = Pedalo.find(params[:pedalo_id])
+    @reservation = Reservation.new
+    # do some JS if time to display the total price
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    @user = current_user
+    @pedalo = Pedalo.find(params[:pedalo_id])
+    @reservation.pedalo_id = @pedalo.id
+    @reservation.user_id = @user.id
+    @reservation.transaction_price = 30
+    if @reservation.save
+      redirect_to pedalo_reservation_path(@pedalo.id, @reservation.id)
+    else
+      render :new
+    end
   end
 
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:start_time, :end_time)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_112754) do
+ActiveRecord::Schema.define(version: 2021_03_23_151445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,13 +29,13 @@ ActiveRecord::Schema.define(version: 2021_03_23_112754) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer "transaction_price"
-    t.date "start_time"
-    t.date "end_time"
     t.string "accepted", default: "pending"
     t.bigint "user_id", null: false
     t.bigint "pedalo_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.time "start_time"
+    t.time "end_time"
     t.index ["pedalo_id"], name: "index_reservations_on_pedalo_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
