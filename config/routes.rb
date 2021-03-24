40c8fdd504cedcb0e11b396ee88684d5ccff4f81
Kilian Hayat <khayat@citycable.ch>
2021-03-24 12:34:40 +0100

    Add a route update to reservation

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     member do
       get 'reservations', to: 'reservations#index_res_pedalos'
     end
-    resources :reservations, only: [:show, :new, :create]
+    resources :reservations, only: [:show, :new, :create, :update]
   end
   get 'reservations', to: 'reservations#index_res_users'
 end
