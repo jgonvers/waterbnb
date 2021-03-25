bd67449a0c7683bc60a8a57b6c83d93e86de1522
maxime jost <maxime.jost@gmail.com>
2021-03-25 15:26:56 +0100

    photo field added to signup form

--- app/models/pedalo.rb
+++ app/models/pedalo.rb
@@ -1,6 +1,7 @@
 class Pedalo < ApplicationRecord
   belongs_to :owner, :class_name => "User"
   has_many :reservations
-  validates :name, :description, :price_per_hour, :location, presence: true
+  validates :name, :description, :price_per_hour, :location, :photo, presence: true
   validates :price_per_hour, numericality: { greater_than_or_equal_to: 15 }
+  has_one_attached :photo
 end
