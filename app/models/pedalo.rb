704f7429fd0fdeff3d4571b80f88ee768f9232a9
Kilian Hayat <khayat@citycable.ch>
2021-03-25 13:50:07 +0100

    add validation and geocoding setting within Pedalo model

--- app/models/pedalo.rb
+++ app/models/pedalo.rb
@@ -1,5 +1,7 @@
 class Pedalo < ApplicationRecord
   belongs_to :owner, :class_name => "User"
   has_many :reservations
-  validates :name, :description, :price_per_hour, presence: true
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+  validates :name, :description, :address, :price_per_hour, presence: true
 end
