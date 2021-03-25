a2baa5f9d55de0b95b314aa3027d07495d8cfa20
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-03-25 11:23:21 +0100

    added route for review, made other routes more explicit

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
   devise_for :users
   root to: 'pages#home'
-  get "/user", to: "users#homepage"
-  resources :pedalos, only: [:index, :show, :new, :create] do
-    member do
-      get 'reservations', to: 'reservations#index_res_pedalos'
-    end
-    resources :reservations, only: [:show, :new, :create, :update]
-  end
-  get 'reservations', to: 'reservations#index_res_users'
+  # get "/users", to: "users#homepage"
+  resources :pedalos, only: [:index, :show, :new, :create]
+  
+  get "/pedalos/:pedalo_id/reservations", to: "reservations#index_res_pedalos", as: :pedalo_reservations
+  get "/user/reservations", to: "reservations#index_res_users", as: :user_reservations
+  get "/reservations/:id", to: "reservations#show", as: :reservation
+  get "/pedalos/:pedalo_id/reservations/new", to: "reservations#new", as: :new_reservation
+  post "/reservations/:id", to: "reservations#create"
+  patch "/reservations/:id", to: "reservations#update"
 
-  get "/reservations/:reservation_id/reviews/new", to: "reviews#new"
+  #   member do
+  #     get 'reservations', to: 'reservations#index_res_pedalos'
+  #   end
+  #   resources :reservations, only: [:show, :new, :create, :update]
+  # end
+  # get 'reservations', to: 'reservations#index_res_users'
+
+  get "/reservations/:reservation_id/reviews/new", to: "reviews#new", as: :new_review
   post "/pedalos/:pedalo_id", to: "reviews#create"
 end
