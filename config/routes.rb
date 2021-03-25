c670067ea56d16c28a11af8aac3399cd4a191ee2
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-03-25 10:21:28 +0100

    created reviews controller with create and new, added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
   devise_for :users
   root to: 'pages#home'
   get "/user", to: "users#homepage"
@@ -9,4 +11,7 @@ Rails.application.routes.draw do
     resources :reservations, only: [:show, :new, :create, :update]
   end
   get 'reservations', to: 'reservations#index_res_users'
+
+  get "/reservations/:reservation_id/reviews/new", to: "reviews#new"
+  post "/pedalos/:pedalo_id", to: "reviews#create"
 end
