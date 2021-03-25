23f2ed2395b28524afc559b1a2638aeef48c191a
maxime jost <maxime.jost@gmail.com>
2021-03-25 11:35:27 +0100

    pedalo creation form frontend

--- app/controllers/pedalos_controller.rb
+++ app/controllers/pedalos_controller.rb
@@ -9,9 +9,15 @@ class PedalosController < ApplicationController
   end
 
   def new
+    @pedalo = Pedalo.new
   end
 
   def create
   end
 
+  private
+
+  def pedalo_params
+    params.require(:pedalo).permit(:name, :description, :price_per_hour, :photo)
+  end
 end
--- app/models/pedalo.rb
+++ app/models/pedalo.rb
@@ -1,5 +1,6 @@
 class Pedalo < ApplicationRecord
   belongs_to :owner, :class_name => "User"
   has_many :reservations
-  validates :name, :description, :price_per_hour, presence: true
+  validates :name, :description, :price_per_hour, :photo, presence: true
+  validates :price_per_hour, numericality: { greater_than_or_equal_to: 15 }
 end
