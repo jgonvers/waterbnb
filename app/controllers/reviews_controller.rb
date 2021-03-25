25e5297c220ddfcc917a36186434893e826179d2
Joachim Gonvers <joachim.gonvers@gmail.com>
2021-03-25 11:46:45 +0100

    new/create backend of review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,21 @@
 class ReviewsController < ApplicationController
   def new
+    @new_review = Review.new
   end
 
   def create
+    @review = Review.new(review_params)
+    @review.reservation_id = params[:reservation_id]
+    if @review.save
+      redirect_to pedalo_path(Reservation.find(params[:reservation_id]).pedalo)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
